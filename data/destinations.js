6585aa6bcdb0c404f624332a67aa491a48e759b8
Patrick Wilson <pmwilson333@gmail.com>
2020-08-12 16:39:46 -0700

    backend running with destinations

---
+++ data/destinations.js
@@ -0,0 +1,37 @@
+module.exports = [
+  {
+    id: 1,
+    country: 'Costa Rica',
+    city: 'San Jose',
+    flight_hours: 14,
+    need_passport: 1,
+  },
+  {
+    id: 2,
+    country: 'New Zealand',
+    city: 'Auckland',
+    flight_hours: 20,
+    need_passport: 1,
+  },
+  {
+    id: 3,
+    country: 'Japan',
+    city: 'Tokyo',
+    flight_hours: 31,
+    need_passport: 1,
+  },
+  {
+    id: 4,
+    country: 'Switzerland',
+    city: 'Zurich',
+    flight_hours: 15,
+    need_passport: 1,
+  },
+  {
+    id: 5,
+    country: 'United States',
+    city: 'Honolulu',
+    flight_hours: 7,
+    need_passport: 0,
+  }
+];
