7f32a6e844c52b2263ef63c7e013699015de7695
Patrick Wilson <pmwilson333@gmail.com>
2020-08-12 17:06:15 -0700

    fixed test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -28,35 +28,35 @@ describe('app routes', () => {
         country: 'Costa Rica',
         city: 'San Jose',
         flight_hours: 14,
-        need_passport: 1,
+        need_passport: true,
       },
       {
         id: 2,
         country: 'New Zealand',
         city: 'Auckland',
         flight_hours: 20,
-        need_passport: 1,
+        need_passport: true,
       },
       {
         id: 3,
         country: 'Japan',
         city: 'Tokyo',
         flight_hours: 31,
-        need_passport: 1,
+        need_passport: true,
       },
       {
         id: 4,
         country: 'Switzerland',
         city: 'Zurich',
         flight_hours: 15,
-        need_passport: 1,
+        need_passport: true,
       },
       {
         id: 5,
         country: 'United States',
         city: 'Honolulu',
         flight_hours: 7,
-        need_passport: 0,
+        need_passport: false,
       }
     ];
 
@@ -65,6 +65,6 @@ describe('app routes', () => {
       .expect('Content-Type', /json/)
       .expect(200);
 
-    expect(data.body).toEqual(expectation);
+    expect(data.body).deepEqual(expectation);
   });
 });
